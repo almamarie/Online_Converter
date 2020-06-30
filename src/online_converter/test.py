280f1d0178955120159fb41d2a112f0ae46e06a6
almamarie <alouismariea97@gmail.com>
2020-06-30 20:39:19 +0000

    updated test and tried to use django.test but it kept failing. Also changed the structure of the tests

--- src/online_converter/test.py
+++ src/online_converter/test.py
@@ -4,23 +4,48 @@ import backend as lb
 class Test_Backend(unittest.TestCase):
 
     def test_weightConverter(self):
-        print('Weight Converter')
+        print('Weight Converter test random value')
         self.assertEqual(lb.weightConverter(1, 'kg', 'g'), 1000)
 
 
-    def test_lengthConverter(self):
-        print('Length Converter')
-        self.assertEqual(lb.lengthConverter(1, 'km', 'm'), 1000)
+    def test_areaConverterNone(self):
+        print('Area Converter test for return none')
+        self.assertEqual(lb.areaConverter(1, 'square kilometerm', 'square meter'), None)
+    
 
 
-    def test_temperatureConverter(self):
-        print('Temp COnverter')
-        self.assertEqual(lb.temperatureConverter(0, 'cel', 'fah'), 32)
-
-
+class Test_Area(unittest.TestCase):
+    
     def test_areaConverter(self):
-        print('Area COnverter')
+        print('Area Cnverter, test random value')
         self.assertEqual(lb.areaConverter(1, 'square km', 'square meter'), 1000000)
+
+    def test_areaConverterNone(self):
+        print('Area Converter test for return none')
+        self.assertEqual(lb.areaConverter(1, 'square kilometerm', 'square meter'), None)
+        
+        
+        
+class Test_Length(unittest.TestCase):
+    
+    def test_lengthConverter(self):
+        print('Length Converter test for random value')
+        self.assertEqual(lb.lengthConverter(1, 'km', 'm'), 1000)
+    
+ 
+
+class Test_temp(unittest.TestCase):
+    
+    def test_temperatureConverter(self):
+        print('Temp Converter test for none value')
+        self.assertEqual(lb.temperatureConverter(0, 'cel', 'fah'), 32)
+        
+        
+# class Test_form(unittest.TestCase):
+# 	form = forms.convertForm(request.POST or None)
+
+# 	def test_form_is_valid(self):
+#    		self.assertTrue(form.isvalid())
 
 
 
